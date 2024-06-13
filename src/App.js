c39213c345a212f7c4c4d2200de4464eb5bb03e8
SabeehaKT <kampalasabeeha@gmail.com>
2024-06-13 06:57:26 +0530

    view created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddShoe from './Components/AddShoe';
 import SearchShoe from './Components/SearchShoe';
+import ViewShoe from './Components/ViewShoe';
 
 function App() {
   return (
     <div>
     <AddShoe/>
     <SearchShoe/>
+    <ViewShoe/>
     </div>
   );
 }
