3fe7522eb4d70524b46c01e60546dbf09683f2bb
SabeehaKT <kampalasabeeha@gmail.com>
2024-06-13 06:51:07 +0530

    search created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddShoe from './Components/AddShoe';
+import SearchShoe from './Components/SearchShoe';
 
 function App() {
   return (
     <div>
     <AddShoe/>
+    <SearchShoe/>
     </div>
   );
 }
